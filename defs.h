b9cf3763357975744457aa256a433885a2608417
Nikhil Gupta <nikhil062000@gmail.com>
2023-07-14 02:55:47 +0530

    Board Structure

--- defs.h
+++ defs.h
@@ -23,4 +23,26 @@ enum {
 
 enum { FALSE, TRUE };
 
+typedef struct {
+  int pieces[BRD_SQ_NUM];
+  U64 pawns[3];
+
+  int KingSq[2];
+
+  int side;
+  int enPas;
+  int fiftyMove;
+
+  int ply;
+  int hisPly;
+
+  U64 posKey;
+
+  int pceNum[13];
+  int bigPce[3];
+  int majPce[3];
+  int minPce[3];
+
+} S_BOARD;
+
 #endif
